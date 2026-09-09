1fac13428195d68dae8c7af6192ebc01ab04b7d4
OrbitalNumberGenerator <74796877+Orbitalrng@users.noreply.github.com>
2026-09-09 16:43:28 +0300

    Add local_assets_server_test_plus.dart file

--- test/local_assets_server_test_plus.dart
+++ test/local_assets_server_test_plus.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:local_assets_server/local_assets_server.dart';
+import 'package:local_assets_server_plus/local_assets_server_plus.dart';
 
 void main() {
   test('adds one to input values', () {
